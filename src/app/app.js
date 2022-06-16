21f8e3528e293fa30ec3dfd58e7903a91b4aa626
jackXu <xujinzhang@372163.com>
2022-06-16 17:38:47 +0800

    fixed bug

--- src/app/app.js
+++ src/app/app.js
@@ -1,15 +1,14 @@
 const { app, BrowserWindow } = require("electron");
-const {epath} = require("./utils/dir");
-const path = require("path");
+const { epath } = require("./utils/dir");
 
 var win = null;
 app.whenReady().then(() => {
   win = new BrowserWindow({
     width: 1280,
     height: 800,
-    webPreferences:{
-      preload: epath(["app","preload","main.js"])
-    }
+    webPreferences: {
+      preload: epath(["app", "preload", "main.js"]),
+    },
   });
   win.loadFile(epath(["web", "index.html"]));
   win.webContents.openDevTools();
