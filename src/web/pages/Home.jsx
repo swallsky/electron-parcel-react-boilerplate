b7dac1ee7871fcfe30a2b4c2068735dd513be1bb
jackXu <xujinzhang@372163.com>
2022-06-17 18:04:03 +0800

    jsx

--- src/web/pages/Home.jsx
+++ src/web/pages/Home.jsx
@@ -4,12 +4,11 @@ export default function Home() {
   // 首页
   useEffect(()=>{
     window.electronApi.open();
-    console.log("2222 --- spring");
   });
 
   return (
     <>
-      <div>hello world sky</div>
+      <div>hello world sky!!!</div>
     </>
   );
 }
