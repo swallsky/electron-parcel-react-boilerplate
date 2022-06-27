b6e3565ffae1844d78aa514e9a88287c123e8c73
jackXu <xujinzhang@372163.com>
2022-06-27 17:41:27 +0800

    log app fixed

--- src/app/app.js
+++ src/app/app.js
@@ -2,13 +2,10 @@ const { app, BrowserWindow } = require("electron");
 require("./libs/dir").createHomePath();
 const { mainWindow, trayIcon } = require("./main");
 const ipcManager = require("./ipc");
-const log = require("./libs/log");
 
 var win = null;
 var iconTary = null;
 
-log.file("app.js");
-log.trace("start");
 //启动主进程
 app.whenReady().then(() => {
   ipcManager(); //启动主进程通讯
